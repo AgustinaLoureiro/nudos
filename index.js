b00a0220459ab769d5ea1494be9030e54b50ed2e
Agustina Loureiro <agusloureiro@hotmail.com>
2023-04-18 18:16:47 -0300

    funcionando

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Articulo {
 }
 
 const tapizValencia = new Articulo ('TS01', 'tapices', './img/01a_tapiz_simple_20.jpg', 'Tapiz Valencia 20cm', 1800)
-const tapizMalaga = new Articulo ('TS02', 'tapices', './img/01a_tapiz_simple_40.jpg', 'Tapiz Málaga 40cm', 4000)
+const tapizMalaga = new Articulo ('TS02', 'tapices', './img/01b_tapiz_simple_40.jpg', 'Tapiz Málaga 40cm', 4000)
 const tapizSevilla = new Articulo ('TS03', 'tapices', './img/01c_tapiz_simple_60.jpg', 'Tapiz Sevilla 60cm', 6000)
 const tapizToledo = new Articulo ('TS04', 'tapices', './img/01d_tapiz_simple_80.jpg', 'Tapiz Toledo 80cm', 10000)
 const tapizZaragoza = new Articulo ('TS05', 'tapices', './img/01e_tapiz_simple_100.jpg', 'Tapiz Zaragoza 100cm', 15000)
@@ -53,28 +53,31 @@ tapices.forEach((tapiz) => {
     const undSeleccionadas = document.getElementById(`seleccionUnd${tapiz.id}`);
     const disminuirUnd = document.getElementById(`eliminar${tapiz.id}`);
     const incrementarUnd = document.getElementById(`agregar${tapiz.id}`);
-    
+
     disminuirUnd.onclick = () => {
-    if (numero > 0) {
-        numero--;
-        undSeleccionadas.innerText = numero;
+        if (numero > 0) {
+            numero--;
+            undSeleccionadas.innerText = numero;
+        }
     }
-    };
 
     incrementarUnd.onclick = () => {
-    numero++;
-    undSeleccionadas.innerText
-const anadirProd= document.getElementById(`alCarrito${tapiz.id}`)
-anadirProd.onclick=()=>{
-    if(numero !==0){
-        carrito.push(tapiz)
-        console.log(carrito)
-    } else {
-        alert('Ingrese una cantidad correcta')
+        numero++;
+        undSeleccionadas.innerText
+        const anadirProd= document.getElementById(`alCarrito${tapiz.id}`)
+        anadirProd.onclick=()=>{
+            if(numero !==0){
+                carrito.push(tapiz)
+                console.log(carrito)
+            } else {
+                alert('Ingrese una cantidad correcta')
+            }
+        }
     }
-}
 
-const finalizar =document.getElementById('cerrarCarrito')
+})
+
+const finalizar = document.getElementById('cerrarCarrito')
 finalizar.onclick = () => {
     if (carrito.length !== 0) {
         let totalcompra = 0
